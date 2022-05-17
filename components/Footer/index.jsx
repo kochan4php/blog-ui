eeb5b7b617d038add4999571e4f75987b4b9c28e
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-05-17 19:25:25 +0700

    memperbaiki typography pada footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="text-slate-400 flex justify-center items-center pt-8 pb-16">
+  <footer className="text-slate-400 text-lg md:text-xl flex justify-center items-center pt-8 pb-16">
     <p>Copyright (c) 2021 - Design By Nauval</p>
   </footer>
 );
