b50a576a3b6f0dbbb37007e76b4c94cfb0e60554
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-05-17 16:37:05 +0700

    memperbaiki navbar di ukuran layar mobile, md dan lg

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 const Navbar = () => (
-  <header className="bg-transparent w-full py-6 mb-2">
+  <header className="bg-transparent w-full py-6 mb-2 md:mb-6">
     <div className="container">
-      <div className="flex justify-between items-center w-full relative">
+      <div className="flex justify-between items-center w-full relative px-1 md:px-0">
         <button
           className="lg:hidden text-slate-300 text-lg cursor-pointer hover:text-slate-400 transiton duration-300"
           id="hamburger"
@@ -23,59 +23,61 @@ const Navbar = () => (
         </button>
         <div className="flex">
           <nav
-            className="hidden absolute lg:static lg:flex bg-slate-800 lg:bg-transparent lg:border-0 items-center lg:w-[650px] xl:w-[800px] text-lg top-16 border border-slate-500 w-[200px] shadow-md lg:shadow-none md:w-[400px] shadow-slate-400 left-0 rounded-md text-center"
+            className="hidden absolute lg:static lg:flex bg-slate-800 lg:bg-transparent lg:border-0 items-center lg:w-[660px] xl:w-[750px] text-lg top-14 border border-slate-500 w-[270px] shadow-md lg:shadow-none md:w-[350px] shadow-slate-500 left-0 rounded-md text-center"
             id="nav-menu"
           >
             <ul className="flex flex-col lg:flex-row justify-between items-center w-full">
-              <li className="p-6 md:p-8 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200">
-                <div className="flex justify-between items-center mr-8">
-                  <div className="w-[38px] h-[38px] flex justify-center items-center rounded bg-slate-500 text-slate-100 mr-3 text-xl p-1">
-                    E
+              <li className="px-6 py-4 md:px-8 md:py-5 lg:p-0 text-slate-300 hover:text-slate-400 group transition-colors duration-200">
+                <button className="flex items-center justify-center gap-1 hover:text-slate-400">
+                  <div className="flex justify-between items-center">
+                    <div className="w-[35px] h-[35px] flex justify-center items-center rounded bg-slate-500 text-slate-100 mr-3 text-xl p-1">
+                      K
+                    </div>
+                    <p className="text-slate-300 text-xl truncate">
+                      kumikochan
+                    </p>
                   </div>
-                  <p className="text-slate-300 text-xl font-semibold">
-                    Epicetus
-                  </p>
-                </div>
+                </button>
               </li>
-              <li className="p-6 md:p-8 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200">
+              <li className="px-6 py-4 md:px-8 md:py-5 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200 cursor-pointer">
                 <Link href={"/"}>UI Design</Link>
               </li>
-              <li className="p-6 md:p-8 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200">
+              <li className="px-6 py-4 md:px-8 md:py-5 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200 cursor-pointer">
                 <Link href={"/"}>Front End</Link>
               </li>
-              <li className="p-6 md:p-8 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200">
+              <li className="px-6 py-4 md:px-8 md:py-5 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200 cursor-pointer">
                 <Link href={"/"}>Back End</Link>
               </li>
-              <li className="p-6 md:p-8 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200">
-                <Link href={"/"}>Lainnya</Link>
+              <li className="px-6 py-4 md:px-8 md:py-5 lg:p-0 text-slate-300 hover:text-slate-400 transition-colors duration-200 cursor-pointer">
+                <button className="flex items-center justify-center gap-2">
+                  <p>Lainnya</p>
+                </button>
               </li>
             </ul>
           </nav>
         </div>
-        <div className="lg:hidden text-slate-300 cursor-pointer hover:text-slate-400 transiton duration-300 text-sm">
+        <div className="md:hidden text-slate-300 cursor-pointer hover:text-slate-400 transiton duration-300 text-sm">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10 fill-current"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
+            className="h-8 w-8 fill-current"
+            viewBox="0 0 20 20"
+            fill="currentColor"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              fillRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clipRule="evenodd"
             />
           </svg>
         </div>
-        <div className="hidden lg:block">
+        <div className="hidden md:block">
           <form action="">
             <input
               type="text"
               name="search"
               placeholder="Search"
               autoComplete="off"
-              className="px-4 py-2 rounded-full bg-slate-600 text-white focus:ring focus:outline-none"
+              className="px-4 py-1.5 rounded-full bg-slate-600 text-white focus:ring focus:outline-none"
             />
           </form>
         </div>
