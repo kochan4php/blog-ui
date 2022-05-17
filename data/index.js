23b70b22998e3e046bbeabeeb8537605c9135577
Aprodeo Subarno <aprodeosubarno@gmail.com>
2022-05-17 16:45:21 +0700

    menambahkan beberapa data dummy

--- data/index.js
+++ data/index.js
@@ -54,12 +54,12 @@ const blog = [
   },
   {
     id: 3,
-    img: "https://source.unsplash.com/1200x700?internet",
-    category: "internet",
+    img: "https://source.unsplash.com/1200x700?backend",
+    category: "backend",
     date: "June 28, 2021",
-    title: "How to design a product that can grow itself 10x in year",
+    title: "The More Important the Work, the More Important the Rest",
     excerpt:
-      "Auctor Porta. Augue vitae diam mauris faucibus blandit elit per, feugiat leo dui orci. Etiam vestibulum. Nostra netus per conubia dolor.",
+      "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
     detail: `
       <p>Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
       </p>
@@ -80,8 +80,86 @@ const blog = [
   },
   {
     id: 4,
-    img: "https://source.unsplash.com/1200x700?internet",
-    category: "internet",
+    img: "https://source.unsplash.com/1200x700?frontend",
+    category: "frontend",
+    date: "June 28, 2021",
+    title: "Email Love - Email Inspiration, Templates and Discovery",
+    excerpt:
+      "Consider that for a moment: everything we see around us is assumed to have had a cause and is contingent upon something else.",
+    detail: `
+      <p>Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
+      </p>
+      <br />
+      <p>
+      Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.
+      </p>
+      <br />
+      <p>
+      Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.    
+      </p>`,
+    user: {
+      id: 4,
+      avatar: "/img/mugi.jpg",
+      name: "Kotobuki Tsumugi",
+      role: "UI Designer",
+    },
+  },
+  {
+    id: 5,
+    img: "https://source.unsplash.com/1200x700?backend",
+    category: "backend",
+    date: "June 28, 2021",
+    title: "The More Important the Work, the More Important the Rest",
+    excerpt:
+      "Suitable Quality is determined by product users, clients or customers, not by society in general. For example, a low priced product may be viewed as having high.",
+    detail: `
+      <p>Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
+      </p>
+      <br />
+      <p>
+      Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.
+      </p>
+      <br />
+      <p>
+      Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.    
+      </p>`,
+    user: {
+      id: 5,
+      avatar: "/img/mugi.jpg",
+      name: "Kotobuki Tsumugi",
+      role: "UI Designer",
+    },
+  },
+  {
+    id: 6,
+    img: "https://source.unsplash.com/1200x700?design",
+    category: "design",
+    date: "June 28, 2021",
+    title: "Email Love - Email Inspiration, Templates and Discovery",
+    excerpt:
+      "Consider that for a moment: everything we see around us is assumed to have had a cause and is contingent upon something else.",
+    detail: `
+      <p>Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
+      </p>
+      <br />
+      <p>
+      Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.
+      </p>
+      <br />
+      <p>
+      Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.    
+      </p>`,
+    user: {
+      id: 6,
+      avatar: "/img/mugi.jpg",
+      name: "Kotobuki Tsumugi",
+      role: "UI Designer",
+    },
+  },
+  {
+    id: 7,
+    img: "https://source.unsplash.com/1200x700?frontend",
+    category: "frontend",
     date: "June 28, 2021",
     title: "How to design a product that can grow itself 10x in year",
     excerpt:
@@ -98,7 +176,7 @@ const blog = [
       Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.    
       </p>`,
     user: {
-      id: 4,
+      id: 7,
       avatar: "/img/mugi.jpg",
       name: "Kotobuki Tsumugi",
       role: "UI Designer",
